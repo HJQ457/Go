0ac937aebd9233e365168aa3052c2c856a3ca811
HJQ457 <1359069096@qq.com>
2021-12-18 15:10:10 +0800

    2021-12-18

--- src/exec/fundemo03/main/main.go
+++ src/exec/fundemo03/main/main.go
@@ -16,4 +16,39 @@ func main() {
 
 	num := 20
 	utils.Test03(&num)
+
+	a := utils.GetSum
+	fmt.Printf("a的数据类型是%T\nGetSum的数据类型是%T\n", a, utils.GetSum)
+
+	//此处的a和GetSum是等价的
+	res := a(10, 40)
+	fmt.Println("res = ", res)
+
+	res2 := utils.MyFun(utils.GetSum, 50, 60)
+	fmt.Println("res2 = ", res2)
+
+	type myInt int //给int取了别名，在go中myInt和int是两个类型
+
+	var num1 myInt
+	var num2 int
+
+	num1 = 40
+	num2 = int(num1)
+
+	fmt.Println("num = ", num1)
+	fmt.Println("num2 = ", num2)
+
+	res3 := utils.MyFun2(utils.GetSum, 500, 600)
+	fmt.Println("res3 = ", res3)
+
+	//测试可变参数的接收
+	res4 := utils.Sum(10, 0, -1, 90, 10)
+	fmt.Println("res4 = ", res4)
+
+	a1 := 10
+	b1 := 20
+	utils.Swap(&a1, &b1)
+
+	fmt.Printf("a1 = %v,b1 = %v\n", a1, b1)
+
 }
--- src/exec/fundemo03/utils/utils.go
+++ src/exec/fundemo03/utils/utils.go
@@ -17,3 +17,39 @@ func Test03(n1 *int) {
 	*n1 = *n1 + 10
 	fmt.Println("(test03)n1 = ", *n1)
 }
+
+func GetSum(n1 int, n2 int) int {
+	return n1 + n2
+}
+
+//函数是一种基本数据类型，因此在GO中，可以作为形参，并且调用
+func MyFun(funcvar func(int, int) int, num1 int, num2 int) int {
+	return funcvar(num1, num2)
+}
+
+type MyFunType func(int, int) int
+
+func MyFun2(funcvar MyFunType, num1 int, num2 int) int {
+	return funcvar(num1, num2)
+}
+
+//编写函数，求多个int的和
+func Sum(n1 int, args ...int) int {
+	sum := n1
+
+	//遍历args
+	for i := 0; i < len(args); i++ {
+		sum += args[i]
+	}
+
+	return sum
+}
+
+//编写一个函数交换n1和n2的值
+func Swap(n1 *int, n2 *int) {
+	//t := *n1
+	//*n1 = *n2
+	//*n2 = t
+
+	*n1, *n2 = *n2, *n1
+}
